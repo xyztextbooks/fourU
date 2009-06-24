3e9257c83c195d7aabf8a0f09abfd92038323952
xiongchiamiov <xiong.chiamiov@gmail.com>
2009-06-24 14:23:33 -0700

    add licensing information

--- fourU/urls.py
+++ fourU/urls.py
@@ -1,3 +1,19 @@
+################################################################################
+# Copyright (C) 2009 XYZ Textbooks                                             #
+#                                                                              #
+# This file is part of fourU.                                                  #
+#                                                                              #
+# This program is free software; you can redistribute it and/or modify it under#
+# the terms of either: (a) the GNU General Public License as published by the  #
+# Free Software Foundation; either version 3, or (at your option) any later    #
+# version, or (b) the MIT License which comes with this package.               #
+#                                                                              #
+# fourU is distributed in the hope that it will be useful,                     #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See either the         #
+# GNU General Public License or the MIT License for more details.              #
+################################################################################
+
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
