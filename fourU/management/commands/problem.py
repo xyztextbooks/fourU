e1a7fc74447ea3e5a209c833a59849af8b6b8050
xiongchiamiov <xiong.chiamiov@gmail.com>
2009-07-16 12:26:49 -0700

    alter manage.py problem script to never use ipython

--- fourU/management/commands/problem.py
+++ fourU/management/commands/problem.py
@@ -9,11 +9,10 @@ from django.core.management.base import LabelCommand
 from optparse import make_option
 
 class Command(LabelCommand):
-	option_list = LabelCommand.option_list + (
-		make_option('--plain', action='store_true', dest='plain',
-			help='Tells Django to use plain Python, not IPython.'),
-	)
-	help = "Runs a Python interactive interpreter. Tries to use IPython, if it's available."
+	option_list = LabelCommand.option_list
+	help = "Runs a Python interactive interpreter. " + \
+	       "Doesn't use IPython, since it doesn't like sys.exit(), " + \
+	       "and you'll be outputting to a file anyways, or wanting to see straight code."
 
 	requires_model_validation = False
 
@@ -25,44 +24,34 @@ class Command(LabelCommand):
 
 		use_plain = options.get('plain', False)
 
-		try:
-			if use_plain:
-				# Don't bother loading IPython, because the user wants plain Python.
-				raise ImportError
-			import IPython
-			# Explicitly pass an empty list as arguments, because otherwise IPython
-			# would use sys.argv from this script.
-			shell = IPython.Shell.IPShell(argv=[label,])
-			shell.mainloop()
+		import code
+		# Set up a dictionary to serve as the environment for the shell, so
+		# that tab completion works on objects that are imported at runtime.
+		# See ticket 5082.
+		imported_objects = {}
+		try: # Try activating rlcompleter, because it's handy.
+			import readline
 		except ImportError:
-			import code
-			# Set up a dictionary to serve as the environment for the shell, so
-			# that tab completion works on objects that are imported at runtime.
-			# See ticket 5082.
-			imported_objects = {}
-			try: # Try activating rlcompleter, because it's handy.
-				import readline
-			except ImportError:
-				pass
-			else:
-				# We don't have to wrap the following import in a 'try', because
-				# we already know 'readline' was imported successfully.
-				import rlcompleter
-				readline.set_completer(rlcompleter.Completer(imported_objects).complete)
-				readline.parse_and_bind("tab:complete")
+			pass
+		else:
+			# We don't have to wrap the following import in a 'try', because
+			# we already know 'readline' was imported successfully.
+			import rlcompleter
+			readline.set_completer(rlcompleter.Completer(imported_objects).complete)
+			readline.parse_and_bind("tab:complete")
 
-			# We want to honor both $PYTHONSTARTUP and .pythonrc.py, so follow system
-			# conventions and get $PYTHONSTARTUP first then import user.
-			if not use_plain: 
-				pythonrc = os.environ.get("PYTHONSTARTUP") 
-				if pythonrc and os.path.isfile(pythonrc): 
-					try: 
-						execfile(pythonrc) 
-					except NameError: 
-						pass
-				# This will import .pythonrc.py as a side-effect
-				import user
-			#code.interact(local=imported_objects)
-			console = code.InteractiveConsole()
-			codeObject = compile(file(label).read(), '', 'exec')
-			console.runcode(codeObject)
+		# We want to honor both $PYTHONSTARTUP and .pythonrc.py, so follow system
+		# conventions and get $PYTHONSTARTUP first then import user.
+		if not use_plain: 
+			pythonrc = os.environ.get("PYTHONSTARTUP") 
+			if pythonrc and os.path.isfile(pythonrc): 
+				try: 
+					execfile(pythonrc) 
+				except NameError: 
+					pass
+			# This will import .pythonrc.py as a side-effect
+			import user
+		#code.interact(local=imported_objects)
+		console = code.InteractiveConsole()
+		codeObject = compile(file(label).read(), '', 'exec')
+		console.runcode(codeObject)
